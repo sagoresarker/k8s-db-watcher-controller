b20b8aaf3186a4d24d70a46760f532bb20a301d3
sagoresarker <sagoresarker.cse@gmail.com>
2024-10-01 16:14:51 +0600

    add image pull option when not found in locally

--- pkg/docker/launcher.go
+++ pkg/docker/launcher.go
@@ -3,8 +3,11 @@ package docker
 import (
 	"context"
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
 )
 
@@ -23,6 +26,20 @@ func NewLauncher() (*Launcher, error) {
 
 // LaunchContainer creates and starts a new container
 func (l *Launcher) LaunchContainer(ctx context.Context, imageName string) error {
+	// Check if the image exists locally
+	_, _, err := l.client.ImageInspectWithRaw(ctx, imageName)
+	if err != nil {
+		if client.IsErrNotFound(err) {
+			// Image not found locally, attempt to pull it
+			fmt.Printf("Image %s not found locally. Attempting to pull...\n", imageName)
+			if err := l.pullImage(ctx, imageName); err != nil {
+				return fmt.Errorf("failed to pull image %s: %w", imageName, err)
+			}
+		} else {
+			return fmt.Errorf("error inspecting image %s: %w", imageName, err)
+		}
+	}
+
 	resp, err := l.client.ContainerCreate(ctx, &container.Config{
 		Image: imageName,
 	}, nil, nil, nil, "")
@@ -36,6 +53,19 @@ func (l *Launcher) LaunchContainer(ctx context.Context, imageName string) error 
 
 	fmt.Printf("Container started: %s\n", resp.ID)
 	return nil
+}
+
+// pullImage pulls a Docker image from the registry
+func (l *Launcher) pullImage(ctx context.Context, imageName string) error {
+	out, err := l.client.ImagePull(ctx, imageName, image.PullOptions{})
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Print pull progress
+	_, err = io.Copy(os.Stdout, out)
+	return err
 }
 
 // Close closes the Docker client
